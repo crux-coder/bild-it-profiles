b9b2b5492fa241411fef238a4db0d84c2b554b38
Jasmin Mustafic <jasmin@bildbosnia.org>
2019-09-24 14:06:13 +0200

    Ready to deploy(hopefully).

--- bild-it-profiles/server.js
+++ bild-it-profiles/server.js
@@ -7,6 +7,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use(cors());
 app.use(express.json());
 
@@ -25,6 +28,12 @@ const userRouter = require('./routes/users');
 
 app.use('/exercises', exerciseRouter);
 app.use('/users', userRouter);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
