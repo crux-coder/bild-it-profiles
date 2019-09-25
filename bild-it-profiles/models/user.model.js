939fa6d3ade8834fa9f304a6d38a0208263069dd
Jasmin Mustafic <jasmin@bildbosnia.org>
2019-09-25 09:45:45 +0200

    Added log of the error on mongoose connection, and expanded a user model to contain first and last name, dob and username.

--- bild-it-profiles/models/user.model.js
+++ bild-it-profiles/models/user.model.js
@@ -3,16 +3,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        minlength: 3
-    }
+    username: { type: String, required: true, unique: true, trim: true, minlength: 3 },
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    dateOfBirth: { type: Date, required: true }
 }, {
-    timestamps: true
-});
+        timestamps: true
+    });
 
 const User = mongoose.model('User', userSchema);
 
