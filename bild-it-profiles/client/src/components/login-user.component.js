3577173bc401e68dde2f5d822788a9d59dc7d225
Jasmin Mustafic <jasmin@bildbosnia.org>
2019-09-26 16:08:43 +0200

    Small design changes on the login page.

--- bild-it-profiles/client/src/components/login-user.component.js
+++ bild-it-profiles/client/src/components/login-user.component.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import 'react-datepicker/dist/react-datepicker.css';
+import '../App.css';
+import logo from '../images/bildit.png';
 
 export default class Login extends Component {
     constructor(props) {
@@ -35,31 +37,33 @@ export default class Login extends Component {
 
     render() {
         return (
-            <div>
-                <h3>Login</h3>
-                <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label>E-mail: </label>
-                        <input type="email"
-                            required
-                            name="email"
-                            className="form-control"
-                            value={this.state.email}
-                            onChange={this.handleInputChange}
-                        />
-                        <label>Password: </label>
-                        <input type="password"
-                            required
-                            name="password"
-                            className="form-control"
-                            value={this.state.password}
-                            onChange={this.handleInputChange}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <input type="submit" value="Login" className="btn btn-primary" />
-                    </div>
-                </form>
+            <div className="row justify-content-center vertical-center mt-0">
+                <div className="col-4 text-center">
+                    <img src={logo} alt="bildit logo" />
+                    <form onSubmit={this.onSubmit}>
+                        <div className="form-group">
+                            <label>E-mail: </label>
+                            <input type="email"
+                                required
+                                name="email"
+                                className="form-control"
+                                value={this.state.email}
+                                onChange={this.handleInputChange}
+                            />
+                            <label>Password: </label>
+                            <input type="password"
+                                required
+                                name="password"
+                                className="form-control"
+                                value={this.state.password}
+                                onChange={this.handleInputChange}
+                            />
+                        </div>
+                        <div className="">
+                            <button type="submit" className="btn btn-primary login-btn">Login</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
