6e2db0d1f69ec93551ac80dba8f859dadc0edb8e
Jasmin Mustafic <jasmin@bildbosnia.org>
2019-10-01 12:30:54 +0200

    Adding and editing of the exercises are in material design now.

--- bild-it-profiles/client/src/components/exercise.component.js
+++ bild-it-profiles/client/src/components/exercise.component.js
@@ -6,6 +6,7 @@ import TableRow from '@material-ui/core/TableRow';
 import Delete from '@material-ui/icons/Delete';
 import Edit from '@material-ui/icons/Edit';
 import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
 
 export default class Exercise extends Component {
 
@@ -16,7 +17,12 @@ export default class Exercise extends Component {
                 <TableCell component="th" scope="row">
                     {formatDate(props.exercise.date)}
                 </TableCell>
-                {props.user && <TableCell align="left">{props.exercise.user.fullName}</TableCell>}
+                {props.user && <TableCell align="left">
+                    <Typography>
+                        <Link to={`/user/${props.exercise.user._id}`} color="inherit">
+                            {props.exercise.user.fullName}
+                        </Link>
+                    </Typography></TableCell>}
                 <TableCell align="left">{props.exercise.description}</TableCell>
                 <TableCell align="left">{props.exercise.duration}</TableCell>
                 <TableCell align="center">
